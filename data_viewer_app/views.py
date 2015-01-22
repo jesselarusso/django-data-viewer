bdbf592570801b5e36545b22999bee6cfb563d77
Jesse La Russo <jlarusso@canvas.is>
2015-01-22 18:38:15 -0500

    Stub out sort handling

--- data_viewer_app/views.py
+++ data_viewer_app/views.py
@@ -9,14 +9,13 @@ from django.http import JsonResponse
 # ?sort=+name
 # ?query=abcdef&field=name
 def index(request):
-
-    # sort = request.GET.get('sort')
-    # query = request.GET.get('query')
-    # field = request.GET.get('field')
-
     data = load_json()
     data = offset(request, data)
     data = limit(request, data)
+    data = sort(request, data)
+
+    # query = request.GET.get('query')
+    # field = request.GET.get('field')
 
     return JsonResponse({ 'colors': data })
 
@@ -27,16 +26,26 @@ def load_json():
 
 
 def offset(request, data):
-    offset = request.GET.get('offset')
-    if offset is not None:
-        return data[int(offset):len(data)]
+    param = request.GET.get('offset')
+    if param is not None:
+        return data[int(param):len(data)]
     else:
         return data
 
 
 def limit(request, data):
-    limit = request.GET.get('limit')
-    if limit is not None:
-        return data[:int(limit)]
+    param = request.GET.get('limit')
+    if param is not None:
+        return data[:int(param)]
+    else:
+        return data
+
+def sort(request, data):
+    param = request.GET.get('sort')
+    if param is not None:
+        direction = param[0]
+        field = param[1:len(param)]
+
+        # return sorted array
     else:
         return data
